0adfd3e6eadc6b32f9e1404c02a041584ba4421f
Christoph Kraemer <christoph.kraemer@greenbone.net>
2022-06-13 13:57:26 +0200

    Add: workflow for smoketest_lint

--- smoketest_lint/cmd/main.go
+++ smoketest_lint/cmd/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"path/filepath"
 
 	"github.com/greenbone/openvas-scanner/smoketest_lint/test"
 )
 
-var path = "data"
-
 func main() {
 
+	openvasDir := flag.String("openvasDir", "", "location of the openvas-nasl-lint executable")
+	path := flag.String("testFiles", "data", "folder for the nasl test data")
+	flag.Parse()
+
+	openvasExe := filepath.Join(*openvasDir, "openvas-nasl-lint")
+
 	tfs := test.TestFiles()
-	err := filepath.Walk(path, tfs.Parse)
+	err := filepath.Walk(*path, tfs.Parse)
 	if err != nil {
-		fmt.Printf("Unable to parse files in %s: %s\n", path, err)
+		fmt.Printf("Unable to parse files in %s: %s\n", *path, err)
 	}
 
 	for _, tf := range tfs.Tfs {
-		errs := tf.Test()
+		errs := tf.Test(openvasExe)
 		if len(errs) > 0 {
 			fmt.Printf("%d error(s) while processing %s:\n", len(errs), tf.Name)
 			for _, err := range errs {
@@ -26,5 +31,4 @@ func main() {
 			}
 		}
 	}
-
 }
